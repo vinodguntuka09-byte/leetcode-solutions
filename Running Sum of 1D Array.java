c242be00c9f914d50d4037a0d9dd4c57d8f75e2c
Vinod <vinodguntuka09@gmail.com>
2026-06-05 23:09:40 +0530

    Solved Running Sum of 1D Array

---
+++ Running Sum of 1D Array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] runningSum =new int[nums.length];
+        int currentSum = 0;
+        for( int i = 0 ; i < nums.length; i++){
+            currentSum += nums[i];
+            runningSum[i] = currentSum;
+        }
+        return runningSum;
+    }
+}
