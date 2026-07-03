25033175bec6325b329b5d4fcf01e2754940c59c
Vinod <vinodguntuka09@gmail.com>
2026-07-03 07:22:21 +0530

    Solved Height Checker

---
+++ Height Checker.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int count = 0;
+        int[] expected;
+        expected = heights.clone();
+        Arrays.sort(expected);
+        for(int i=0;i<heights.length;i++){
+            if(heights[i]!=expected[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+}
