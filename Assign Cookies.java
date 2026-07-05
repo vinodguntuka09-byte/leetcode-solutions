64448f5b106fe17e0a041c4ee7ad6f99de0c3325
Vinod <vinodguntuka09@gmail.com>
2026-07-05 08:42:21 +0530

    Solved the First greedy problem

---
+++ Assign Cookies.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+         Arrays.sort(g);
+        Arrays.sort(s);
+
+        int child = 0;
+        int cookie = 0;
+        int count = 0;
+
+        while (child < g.length && cookie < s.length) {
+
+            if (s[cookie] >= g[child]) {
+                count++;
+                child++;
+                cookie++;
+            } else {
+                cookie++;
+            }
+        }
+
+        return count;
+    }
+}
