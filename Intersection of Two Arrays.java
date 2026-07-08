1dcb698e58c4ffdc71260534267a6754fb226508
Vinod <vinodguntuka09@gmail.com>
2026-07-08 21:25:13 +0530

    Solved

---
+++ Intersection of Two Arrays.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> answer = new HashSet<>();
+        for(int num:nums1){
+            set.add(num);
+        }
+        for(int num:nums2){
+           if(set.contains(num)){
+            answer.add(num);
+           }
+        }
+        int[] result =new int[answer.size()];
+        int i = 0;
+        for(int num:answer){
+            result[i++] = num;
+        }
+        return result;
+       }
+}
